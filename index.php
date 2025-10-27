21601cb1e67b5ef4a69c5342332f492cc4106ed6
DylanCastro08 <dylandc08022003@gmail.com>
2025-10-27 09:20:37 -0500

    ACTS

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $terceros = $sentencia->fetchAll(PDO::FETCH_OBJ);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/styles.css">
 </head>
 <body>
 
